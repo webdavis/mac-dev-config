6b11240df6d9a27c296202019baf8cef2b78e1a5
Stephen A. Davis <23553256+webdavis@users.noreply.github.com>
2025-10-07 00:07:15 -0600

    Set laptop hostname to `dresden` [details in body]
    Context:
      `nix-darwin` will automatically try to find a configuration whose name
      matches your system’s hostname (as returned by `hostname` or
      `scutil --get LocalHostName` on macOS). Adding the target computer's
      hostname here allows us to avoid providing the name explicitly when
      building or rebuilding the system configuration. For example:
    
    Change:
      Rename the `darwinConfigurations` attribute to the system’s hostname
      (`dresden`) so that `nix-darwin` automatically selects the correct
      configuration during rebuilds
    
      With this change, we can run:
    
      `darwin-rebuild switch --flake .`
    
      instead of needing to specify the hostname explicitly:
    
      `darwin-rebuild switch --flake .#dresden`
    
      This aligns the configuration name with the machine hostname

--- flake.nix
+++ flake.nix
@@ -35,8 +35,8 @@
   in
   {
     # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."simple" = nix-darwin.lib.darwinSystem {
+    # $ darwin-rebuild build --flake .#dresden
+    darwinConfigurations."dresden" = nix-darwin.lib.darwinSystem {
       modules = [ configuration ];
     };
   };
